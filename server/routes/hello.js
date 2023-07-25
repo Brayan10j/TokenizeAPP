86d2b73bd77608bce6778475721f6af5c14df0f8
Brayan <brayanespitia10@gmail.com>
2023-07-25 10:21:45 -0500

    fix

--- server/routes/hello.js
+++
@@ -1,6 +0,0 @@
-import { serverSupabaseClient } from "#supabase/server";
-export default defineEventHandler(async (event) => {
-  const client = serverSupabaseClient(event);
-  const { data } = await client.from("products").select("data");
-  return { products: data };
-});
